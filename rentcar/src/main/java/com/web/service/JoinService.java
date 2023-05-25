179e78417da04d6317191f0b1fff4b74586b375a
Kim Seonghoon <sung5139@naver.com>
2023-05-25 11:42:55 +0900

    로그아웃, 유효성 검사 추가

--- rentcar/src/main/java/com/web/service/JoinService.java
+++ rentcar/src/main/java/com/web/service/JoinService.java
@@ -13,24 +13,12 @@ import lombok.RequiredArgsConstructor;
 public class JoinService {
 	   @Autowired
 	   private UserRepository userRepository;
-//	   @Autowired
-//	   BCryptPasswordEncoder passwordEncoder;
-//	   
-//	   public String rawPw;
-//	   public String encodedPw;
-	   
+
 	   public void join(User user) {
-//		   rawPw = user.getPassword();
-//		   encodedPw = passwordEncoder.encode(user.getPassword());
-//		   user.setPassword(encodedPw);
 		   userRepository.save(user);
 	   }
-	   
-//	   public boolean athenticate(String rawPw, String encodedPw) {
-//		   return passwordEncoder.matches(rawPw, encodedPw);
-//	   }
-	   
-	    public boolean isIdDuplicated(String id) {
-	    	return userRepository.existsById(id);
+	   public boolean isIdDuplicated(String id) {
+		   User user = userRepository.findById(id);
+		   return user != null;
 	    }
 }
