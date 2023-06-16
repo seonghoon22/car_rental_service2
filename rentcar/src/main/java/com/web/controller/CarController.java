d56141474fef596d6fd5956997fbde742f858b92
Kim Seonghoon <sung5139@naver.com>
2023-06-16 10:08:07 +0900

    대여시간 계산 로직 추가

--- rentcar/src/main/java/com/web/controller/CarController.java
+++ rentcar/src/main/java/com/web/controller/CarController.java
@@ -8,8 +8,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.DigestUtils;
-import org.springframework.util.ResourceUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -17,16 +15,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.web.domain.Car;
 import com.web.service.CarService;
 
-import lombok.Getter;
-  
 @Controller
-@RequestMapping("/cars")
+//@RequestMapping("/cars")
 public class CarController {
-    
+
     private CarService carService;
 
     @Autowired
@@ -35,7 +30,7 @@ public class CarController {
         this.carService = carService;
     }
 
-    @GetMapping
+    @GetMapping("/cars")
     public String getAllCars(Model model) {
        System.out.println("@GetMapping(\"/cars\")");
         List<Car> cars = carService.getAllCars();
@@ -43,16 +38,14 @@ public class CarController {
         return "car-list";
     }
 
-    @GetMapping("/new")
+    @GetMapping("/cars/new")
     public String showCarForm(Model model) {
-
        System.out.println("@GetMapping(\"/cars\")22");
-
         model.addAttribute("car", new Car());
-    	return "car-form";
+        return "car-form";
     }
-    /*
-    @PostMapping("/new")
+
+    @PostMapping("/cars/new")
     public String saveCar(@Valid @ModelAttribute Car car, BindingResult bindingResult,
                           @RequestParam("file") MultipartFile file) throws Exception {
        System.out.println("@GetMapping(\"/cars\")33");
@@ -63,51 +56,12 @@ public class CarController {
         carService.saveCar(car,file);
         return "redirect:/cars";
     }
-    */
 
-    @PostMapping("/new")
-    public String saveCar(@Valid @ModelAttribute Car car, BindingResult bindingResult,
-                          @RequestParam("file") MultipartFile file) throws IOException {
-    	System.out.println("@GetMapping(\"/cars\")이미지 ");
-        if (bindingResult.hasErrors()) {
-            return "car-form";
-        }
-
-        if (!file.isEmpty()) {
-      
-        	// 파일 저장 경로
-  //          String filePath = "src/main/resources/static/images/" + file.getOriginalFilename();
- 
-        	
-
-            // 프로젝트의 실제 루트 경로를 가져옵니다.
-            String projectRootPath = ResourceUtils.getFile("src/main/resources/static/images/").getAbsolutePath();
-          
-            // 파일 저장 경로
-            String filePath = projectRootPath + "/" + file.getOriginalFilename();
-
-            // 이미지 경로 저장
-            car.setImgpath(filePath);
-            
-            System.out.println("filePath : " + filePath);
-            // file:src/main/resources/static/images/
-            
-            // 파일을 저장
-            file.transferTo(new File(filePath));
-            
-        }
-    
-        carService.saveCar(car);
-        return "redirect:/cars";
-    }
-  
-    @GetMapping("/{carNo}")
+    @GetMapping("/cars/{carNo}")
     public String getCarDetails(@PathVariable("carNo") Long car_no, Model model) {
        System.out.println("@GetMapping(\"/cars\")44");
         Car car = carService.getCarByCar_no(car_no);
         model.addAttribute("car", car);
         return "car-details";
     }
-    
 }
-
--- rentcar/src/main/java/com/web/controller/SearchController.java
+++ rentcar/src/main/java/com/web/controller/SearchController.java
@@ -1,10 +1,15 @@
 package com.web.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.web.domain.Car;
+import com.web.service.CarService;
 import com.web.service.CarServiceImpl;
 
 
@@ -12,11 +17,17 @@ import com.web.service.CarServiceImpl;
 public class SearchController {
 	
 	@Autowired
-	private CarServiceImpl s;
+	private CarService carService;
 	
 	@RequestMapping(value="/search", method=RequestMethod.GET)
 	public String search() {
 		return "search";
 	}
-
+	
+	@RequestMapping(value="/search", method=RequestMethod.POST)
+	public String searchAvailable(Model model) {
+		List<Car> cars = carService.getAllCars();
+        model.addAttribute("cars", cars);
+		return "search";
+	}
 }
