4ea1893049c971ef4dba92f7a16a7b71c4e1c1a6
Kim Seonghoon <sung5139@naver.com>
2023-06-19 15:09:10 +0900

    .

--- rentcar/src/main/java/com/web/repository/CarRepository.java
+++ rentcar/src/main/java/com/web/repository/CarRepository.java
@@ -12,11 +12,11 @@ import com.web.domain.Car;
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
 
+
+
 	 @Query("SELECT c FROM Car c WHERE NOT EXISTS (SELECT r FROM Rental r WHERE c.car_no = r.car_no AND r.endDate >= :start AND r.startDate <= :end)")
 	List<Car> findAvailableCars(@Param("start") Date startDate, 
                  @Param("end") Date endDate);
-	 
-//	 @Query("SELECT c FROM Car c WHERE NOT EXISTS (SELECT r FROM Rental r WHERE c.car_no = r.car_no AND r.endDate >= ?1 AND r.startDate <= ?2)")
-//	 List<Car> findAvailableCars(LocalDate startDate, LocalDate endDate);
-}
 
+
+}
